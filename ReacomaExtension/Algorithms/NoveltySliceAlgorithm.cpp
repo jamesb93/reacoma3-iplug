31e2e71312abd404153659938b0fe4e43009f625
James Bradbury <gh@jbry.me>
2025-05-25 19:36:25 +0800

    encapsulate processing into noveltyslice algorithm

--- ReacomaExtension/Algorithms/NoveltySliceAlgorithm.cpp
+++ ReacomaExtension/Algorithms/NoveltySliceAlgorithm.cpp
@@ -1,23 +1,25 @@
 // NoveltySliceAlgorithm.cpp
 #include "NoveltySliceAlgorithm.h"
 #include "ReacomaExtension.h" // Include the full definition of ReacomaExtension
-#include "ReaperExt_include_in_plug_hdr.h" // For MediaItem*, PCM_source*, etc.
-// reaper_plugin_functions.h is likely not needed directly here if ReacomaExtension wraps calls
+#include "reaper_plugin_functions.h"
 
 #include "../../dependencies/flucoma-core/include/flucoma/clients/rt/NoveltySliceClient.hpp"
 #include "../../dependencies/flucoma-core/include/flucoma/clients/common/FluidContext.hpp"
 #include "../VectorBufferAdaptor.h" // Make sure this path is correct
 
+using namespace fluid;
+using namespace client;
+
 struct NoveltySliceAlgorithm::Impl {
-    fluid::client::FluidContext mFluCoMaContext;
-    fluid::client::NRTThreadingNoveltySliceClient::ParamSetType mFluCoMaParams;
-    fluid::client::NRTThreadingNoveltySliceClient mFluCoMaClient;
+    FluidContext mContext;
+    NRTThreadingNoveltySliceClient::ParamSetType mParams;
+    NRTThreadingNoveltySliceClient mClient;
     ReacomaExtension* mApiProvider; // Changed to ReacomaExtension*
 
     Impl(ReacomaExtension* apiProvider) : // Changed
-        mFluCoMaContext{},
-        mFluCoMaParams{fluid::client::NRTThreadingNoveltySliceClient::getParameterDescriptors(), fluid::FluidDefaultAllocator()},
-        mFluCoMaClient{mFluCoMaParams, mFluCoMaContext},
+        mContext{},
+        mParams{NRTThreadingNoveltySliceClient::getParameterDescriptors(), FluidDefaultAllocator()},
+        mClient{mParams, mContext},
         mApiProvider(apiProvider) // Changed
     {
         // Any further initialization
@@ -27,20 +29,97 @@ struct NoveltySliceAlgorithm::Impl {
         if (!item || !mApiProvider) return false; // Check if pointer is valid
 
         // Use the ReacomaExtension pointer to call API functions
-        MediaItem_Take* take = mApiProvider->GetActiveTake(item);
+        MediaItem_Take* take = GetActiveTake(item);
         if (!take) return false;
 
-        PCM_source* source = mApiProvider->GetMediaItemTake_Source(take);
+        PCM_source* source = GetMediaItemTake_Source(take);
         if (!source) return false;
 
-        int sampleRate = mApiProvider->GetMediaSourceSampleRate(source);
-        int numChannels = mApiProvider->GetMediaSourceNumChannels(source);
-        double itemLength = mApiProvider->GetMediaItemInfo_Value(item, "D_LENGTH");
-        double playrate = mApiProvider->GetMediaItemTakeInfo_Value(take, "D_PLAYRATE");
-
-        // ... rest of your FluCoMa processing ...
-        // mApiProvider->ShowConsoleMsg("Processing...\n"); // Example of calling another ReacomaExtension method
-
+        int sampleRate = GetMediaSourceSampleRate(source);
+        int numChannels = GetMediaSourceNumChannels(source);
+        double itemLength = GetMediaItemInfo_Value(item, "D_LENGTH");
+        double playrate = GetMediaItemTakeInfo_Value(take, "D_PLAYRATE");
+        double takeOffset = GetMediaItemTakeInfo_Value(take, "D_STARTOFFS");
+    
+        int outSize = sampleRate * std::min(itemLength * playrate, source->GetLength() - takeOffset);
+    
+        std::vector<double> output;
+        output.resize(outSize);
+    
+        PCM_source_transfer_t transfer;
+    
+        transfer.time_s = takeOffset;
+        transfer.samplerate = sampleRate;
+        transfer.nch = 1;
+        transfer.length = outSize;
+        transfer.samples = output.data();
+        transfer.samples_out = 0;
+        transfer.midi_events = nullptr;
+        transfer.approximate_playback_latency = 0.0;
+        transfer.absolute_time_s = 0.0;
+        transfer.force_bpm = 0.0;
+    
+        if (numChannels == 1) {
+            source->GetSamples(&transfer);
+        }
+        // Setup parameters
+        std::vector<float> output_float(output.size());
+        std::transform(output.begin(), output.end(), output_float.begin(),
+        [](double d) { return static_cast<float>(d); });
+        auto inputBuffer = InputBufferT::type(new fluid::VectorBufferAdaptor(output_float, numChannels, outSize, sampleRate));
+    
+        int estimatedSlices = static_cast<int>(output.size() / 1024);
+        auto outBuffer = std::make_shared<MemoryBufferAdaptor>(1, estimatedSlices, sampleRate);
+        auto outputBuffer = BufferT::type(outBuffer);
+    
+        auto threshold = mApiProvider->GetParam(0)->Value();
+        auto filtersize = mApiProvider->GetParam(1)->Value();
+        auto algorithm = mApiProvider->GetParam(2)->Value();
+        
+        
+        mParams.template set<0>(std::move(inputBuffer), nullptr);  // source buffer
+        mParams.template set<1>(LongT::type(0), nullptr);         // startFrame
+        mParams.template set<2>(LongT::type(-1), nullptr);        // numFrames (-1 = all)
+        mParams.template set<3>(LongT::type(0), nullptr);         // startChan
+        mParams.template set<4>(LongT::type(-1), nullptr);        // numChans (-1 = all)
+        mParams.template set<5>(std::move(outputBuffer), nullptr); // indices buffer
+        mParams.template set<6>(LongT::type(algorithm), nullptr);       // algorithm
+        mParams.template set<7>(LongRuntimeMaxParam(3, 3), nullptr); // kernelSize
+        mParams.template set<8>(FloatT::type(threshold), nullptr); // threshold
+        mParams.template set<9>(LongRuntimeMaxParam(3, 3), nullptr); // filterSize
+        mParams.template set<10>(LongT::type(2), nullptr);   // minSliceLength
+        mParams.template set<11>(fluid::client::FFTParams(1024, -1, -1), nullptr); // hopSize
+    
+        mClient = NRTThreadingNoveltySliceClient(mParams, mContext);
+        mClient.setSynchronous(true);
+        mClient.enqueue(mParams);
+        Result result = mClient.process();
+    
+        BufferAdaptor::ReadAccess reader(outputBuffer.get());
+        auto view = reader.samps(0);
+    
+        Undo_BeginBlock2(0);
+    
+        int markerCount = GetNumTakeMarkers(take);
+        for (int i = markerCount - 1; i >= 0; i--) {
+            DeleteTakeMarker(take, i);
+        }
+    
+        int numMarkers = 0;
+        const char* markerLabel = "";
+    
+        for (fluid::index i = 0; i < view.size(); i++) {
+            if (view(i) > 0) {
+                double markerTime = view(i) / sampleRate / playrate;
+                SetTakeMarker(take, -1, markerLabel, &markerTime, nullptr);
+                numMarkers++;
+            }
+        }
+    
+        UpdateTimeline();
+        Undo_EndBlock2(0, "reacoma", -1);
+    
+        
         return true;
     }
 };
