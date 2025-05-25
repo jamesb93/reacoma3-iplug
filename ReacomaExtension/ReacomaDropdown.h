0a6c4f574fa9682d56496f134650569e06c7d535
James Bradbury <gh@jbry.me>
2025-05-25 16:53:37 +0800

    Revert "remove moot dropdown component"
    This reverts commit 356dc4e9d1e3f256665dca97cb256619e11e7708.

---
+++ ReacomaExtension/ReacomaDropdown.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "IControl.h"
+#include <vector>
+#include <string>
+using namespace iplug::igraphics;
+
+class ReacomaSegmented : public IControl
+{
+    public:
+        ReacomaSegmented(const IRECT& bounds, int paramIdx, const std::vector<std::string>& segmentLabels,
+        const IText& textStyle = DEFAULT_TEXT, const IColor& activeColor = DEFAULT_PRCOLOR,
+        const IColor& inactiveColor = DEFAULT_BGCOLOR);
+        
+        void Draw(IGraphics& g) override;
+        void OnMouseDown(float x, float y, const IMouseMod& mod) override;
+        // Potentially OnMouseOver, OnMouseOut for hover effects on segments
+        
+    private:
+        std::vector<std::string> mSegmentLabels;
+        std::vector<IRECT> mSegmentRects;
+        IText mTextStyle;
+        IColor mActiveColor;
+        IColor mInactiveColor;
+        IColor mBorderColor = COLOR_BLACK;
+        float mCornerRadius = 5.f; // For rounded look
+        
+        void CalculateSegmentRects();
+        int GetSegmentForPos(float x, float y);
+};
+    
